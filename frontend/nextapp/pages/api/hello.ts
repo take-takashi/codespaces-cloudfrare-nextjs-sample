45391121b3506169e7502b61971f8ed1452533a6
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-07 13:57:34 +0000

    fix: api routesをcloudfrare用に変更

--- frontend/nextapp/pages/api/hello.ts
+++ frontend/nextapp/pages/api/hello.ts
@@ -5,9 +5,15 @@ type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function handler(req: NextApiRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 }
